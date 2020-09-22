15666e6954ce29470829a2bf66f971d873333f2a
Brandon Horlacher <bhorlacher@tangynt.com>
2020-09-21 22:49:15 -0600

    adding rocket thruster sound.

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -22,6 +24,17 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            
+        } else
+        {
+            if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
         }
 
         if (!(Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)))
